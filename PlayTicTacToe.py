09b95909d9abed3a66ca5326adb94b5018e49194
rdbch <beche.radu@gmail.com>
2019-01-03 15:58:33 +0200

    PlayTicTacToe update

--- PlayTicTacToe.py
+++ PlayTicTacToe.py
@@ -1,9 +1,21 @@
-from aiAlgos.RLAgent     import      RLAgent
-from game.Player         import      Human_Player, AI_Player
-from game.TicTacToe3D    import      TicTacToe3D 
+from aiAlgos.RLAgent     import     RLAgent
+from game.Player         import     Human_Player, AI_Player
+from game.TicTacToe3D    import     TicTacToe3D 
+from aiAlgos.Negamax     import     NegamaxRand
+from easyAI              import     DUAL, SSS, Negamax
 
-# only usable for a (1,3,3) board
-aiAlgo      =   RLAgent('./models/mix2/train_17500.h5')
+# classical algos /easyAi
+steps       =   4                                               #the number of steps AI thinks in advance
+algos       =   [   DUAL(steps),                                #0 
+                    Negamax(steps),                             #1
+                    NegamaxRand(steps),                         #2 - when states are equal, choose randomly from them
+                    SSS(steps),                                 #3
+                    RLAgent('./models/mix2/train_17500.h5')     #4 - trained on a (1,3,3) board
+                    ]
+
+#select choice from above
+choice      =   0
+aiAlgo      =   algos[choice]
 
 #=================================== PLAYERS =========================================
 pl1     =   AI_Player(aiAlgo)
